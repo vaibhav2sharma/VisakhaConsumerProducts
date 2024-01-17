0e5b3e19dfe211cd90ebb1d0d1ab2497f4e4bb22
vaibhav2sharma <108828319+vaibhav2sharma@users.noreply.github.com>
2024-01-17 16:51:16 +0530

    projectfiles

--- src/app/components/Productcard.jsx
+++ src/app/components/Productcard.jsx
@@ -1,124 +1,72 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Productcard = () => {
-  
+
+const Productcard = ({image , name ,p ,price, discountedprice}) => {
+ 
   
   return (
-    <section className='h-[290vh] md:h-[110vh] w-full relative'>
+    <div  className='hover:rotate-2  md:hover:translate-x-3 duration-700  p-4 shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]'>
+       <Image
+        width={500}
+        height={500}
+        src={image}
+        
+        
+      
+      
+      />
+      {/* product card */}
+      <div className='p-4 text-black/[0.9]'>
+        {/* product name and disc */}
+        <h2  className='uppercase text-gray-500  lg:text-[30px] text-center overflow-hidden font-bold'>
+          {name}
+        </h2>
 
-      <h1 className=' text-center mt-[10px] text-[50px]  font-thin uppercase hover:opacity-75 hover:font-normal duration-700 text-purple-600   '>Our products</h1>
+        <p className='text-gray-500 text-center  text-sm  md:text-sm'>
+         {p}
+        </p>
 
-
-      <div className='flex h-[90%] md:h-[100%] lg:h-[60%] items-center justify-center w-full flex-col md:flex-row absolute lg:top-[20%] gap-10 md:gap-4 md:justify-around '>
-
-      <div className="   h-[80%] w-[60%]  md:h-[750px] md:w-[25%]  hover:rotate-2  md:hover:rotate-6 duration-700 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] p-4">
-        <img src='/visakhadetergent.jpg ' className=' h-[60%] w-full'/>
-        <h2 className='uppercase text-gray-500  lg:text-[30px] text-center overflow-hidden font-bold'>Visakha Detergent</h2>
-        <div  className='flex flex-col md:gap-5 gap-1  justify-center items-center'>
-          <p className='text-gray-500 text-center  text-sm  md:text-sm '>
-          Elevate your laundry experience with Visakha Consumer Products' Our formulations are designed to deliver unmatched cleaning power, leaving your clothes fresh, vibrant, and impeccably clean. 
-          </p>
-          <div className='flex flex-row gap-2'>
-          <h3 className='  text-[15px] lg:text-[30px] font-semibold text-gray-600'>Price:<span className='line-through decoration-green-600 '>₹340</span></h3>
-          <h3 className=' text-[15px] lg:text-[30px] font-semibold text-gray-600'>₹240</h3>
-          </div>
+        {/* product price */}
+        {
           
+        }
 
-          
+        <div className=' flex items-center text-black/[0.5]'>
+          {/* original pricee */}
+            <p className='mr-2 text-lg font-semibold'>Price:₹{discountedprice}</p>
+            {/* discounted price */}
+            
+            {/* show discount only when we have an original price */}
+
+           
+              <>
+              <p className=' text-base font-medium line-through decoration-green-700'>
+              ₹{price}
+
+              </p>
+
+              {/* percentage  */}
+
+             
+
+              
+              </>
+
+
+            
+
+
+            
 
         </div>
 
 
-      </div>
-     
 
-      <div className="   h-[80%] w-[60%]  md:h-[750px] md:w-[25%] hover:rotate-2  md:hover:rotate-6 duration-700 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] p-4">
-        <img src='/vtc.jpg ' className=' h-[60%] w-full'/>
-        <h2 className='uppercase text-gray-500  lg:text-[30px] text-center overflow-hidden font-bold'>Visakha Detergent</h2>
-        <div  className='flex flex-col md:gap-5 gap-1  justify-center items-center'>
-          <p className='text-gray-500 text-center  text-sm  md:text-sm'>
-          Elevate your laundry experience with Visakha Consumer Products' Our formulations are designed to deliver unmatched cleaning power, leaving your clothes fresh, vibrant, and impeccably clean. 
-          </p>
-          <div className='flex flex-row gap-2'>
-          <h3 className='  text-[15px] lg:text-[30px]  font-semibold text-gray-600'>Price:<span className='line-through decoration-green-600 '>₹340</span></h3>
-          <h3 className=' text-[15px] lg:text-[30px] font-semibold text-gray-600'>₹240</h3>
-          </div>
-          
-
-          
-
-        </div>
 
 
       </div>
-
-
-
-      <div className=" h-[80%] w-[60%]  md:h-[750px]   md:w-[25%] hover:rotate-2 md:hover:rotate-6 duration-700 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] p-4">
-        <img src='/vfc.jpg' className=' h-[60%] w-full'/>
-        <h2 className='uppercase text-gray-500  lg:text-[30px] text-center overflow-hidden font-bold'>Visakha Detergent</h2>
-        <div  className='flex flex-col md:gap-5 gap-1  justify-center items-center'>
-          <p className='text-gray-500 text-center  text-sm  md:text-sm'>
-          Elevate your laundry experience with Visakha Consumer Products' Our formulations are designed to deliver unmatched cleaning power, leaving your clothes fresh, vibrant, and impeccably clean. 
-          </p>
-          <div className='flex flex-row gap-2'>
-          <h3 className='  text-[15px] lg:text-[30px] font-semibold text-gray-600'>Price:<span className='line-through decoration-green-600 '>₹340</span></h3>
-          <h3 className=' text-[15px] lg:text-[30px] font-semibold text-gray-600'>₹240</h3>
-          </div>
-          
-
-          
-
-        </div>
-
-
-      </div>
-
-
-
-      <div className=" h-[80%] w-[60%]  md:h-[750px]   md:w-[25%] hover:rotate-2 md:hover:rotate-6 duration-700 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] p-4">
-        <img src='/vls.jpg' className=' h-[60%] w-full'/>
-        <h2 className='uppercase text-gray-500  lg:text-[30px] text-center overflow-hidden font-bold'>Visakha Detergent</h2>
-        <div  className='flex flex-col md:gap-5 gap-1  justify-center items-center'>
-          <p className='text-gray-500 text-center  text-sm  md:text-sm'>
-          Elevate your laundry experience with Visakha Consumer Products' Our formulations are designed to deliver unmatched cleaning power, leaving your clothes fresh, vibrant, and impeccably clean. 
-          </p>
-          <div className='flex flex-row gap-2'>
-          <h3 className='  text-[15px] lg:text-[30px] font-semibold text-gray-600'>Price:<span className='line-through decoration-green-600 '>₹340</span></h3>
-          <h3 className=' text-[15px] lg:text-[30px] font-semibold text-gray-600'>₹240</h3>
-          </div>
-          
-
-          
-
-        </div>
-
-
-      </div>
-
-
-      
-      
-
-     
-
-
-
-
-      
-
-
-      </div>
-
-
-      
-
-
-
-
-
-
-    </section>
+    </div>
   )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer"
 import Herobanner from "./components/Herobanner"
 import Navbar from "./components/Navbar"
+
 import Productcard from "./components/Productcard"
 
 
@@ -13,7 +14,31 @@ export default function Home() {
     <div>
       <Navbar/>
       <Herobanner/>
-      <Productcard/>
+      
+      {/* grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14
+        px-5 md:px-0 flex */}
+      {/* <div> */}
+      {/* <h1 className=' text-center mt-[10px] text-[50px]  font-thin uppercase hover:opacity-75 hover:font-normal duration-700 text-purple-600   '>Our products</h1> */}
+      {/* <Productcard image={'/vfc.jpg'} name={'Visakha Detergents'} p={"Elevate your laundry experience with Visakha Consumer Products' Our formulations are designed to deliver unmatched cleaning power, leaving your clothes fresh, vibrant, and impeccably clean. "} price={"₹340"} discountedprice={"₹240"} /> */}
+
+      {/* </div> */}
+      <h1 className=' text-center mt-[10px] text-[50px]  font-thin uppercase hover:opacity-75 hover:font-normal duration-700 text-purple-600   '>Our products</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 my-14
+        px-5 md:px-0">
+
+         
+          
+          <Productcard image={'/vgc.jpg'} name={'Visakha Glass Cleaner'} p={"Elevate your laundry experience with Visakha Consumer Products' Our formulations are designed to deliver unmatched cleaning power, leaving your clothes fresh, vibrant, and impeccably clean. "} price={"1249"} discountedprice={"900"} />
+          <Productcard image={'/vls.jpg'} name={'Visakha Liquid Soap'} p={"Elevate your laundry experience with Visakha Consumer Products' Our formulations are designed to deliver unmatched cleaning power, leaving your clothes fresh, vibrant, and impeccably clean. "} price={"1249"} discountedprice={"800"} />
+          <Productcard image={'/vtc.jpg'} name={'Visakha Toilet Cleaner'} p={"Elevate your laundry experience with Visakha Consumer Products' Our formulations are designed to deliver unmatched cleaning power, leaving your clothes fresh, vibrant, and impeccably clean. "} price={"949"} discountedprice={"600"} />
+         <Productcard image={'/vfc.jpg'} name={'Visakha Floor Cleaner'} p={"Elevate your laundry experience with Visakha Consumer Products' Our formulations are designed to deliver unmatched cleaning power, leaving your clothes fresh, vibrant, and impeccably clean. "} price={"1249"} discountedprice={"850"} />
+         <Productcard image={'/visakhadetergent.jpg'} name={'Visakha Detergent Powder'} p={"Elevate your laundry experience with Visakha Consumer Products' Our formulations are designed to deliver unmatched cleaning power, leaving your clothes fresh, vibrant, and impeccably clean. "} price={"340"} discountedprice={"240"} />
+
+      </div>
+      
+      
+
+      
       <Footer/>
       
      
