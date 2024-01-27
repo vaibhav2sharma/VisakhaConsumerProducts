fc5a821a32b925d770f25930f6ab26f3a49999e7
vaibhav2sharma <108828319+vaibhav2sharma@users.noreply.github.com>
2024-01-27 18:16:57 +0530

    Add files via upload
    ADED SOME PROPERTIES TO FOOTER AND HERO BANNER

--- src/app/components/Herobanner.jsx
+++ src/app/components/Herobanner.jsx
@@ -2,15 +2,17 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
 import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
+import Carousell from './Carousel';
 
 const Herobanner = () => {
   return (
    <>
-    <section className=' w-full h-[100vh] flex  relative flex-col  sm:flex-row '>
+    <section className=' w-full h-[100vh] flex  relative flex-col  sm:flex-row mb-5 '>
 
         <div className='bg-white w-full md:w-[50%]     text-center justify-center items-center flex flex-col h-[100%]   ' >
 
-          <h1 className='text-black   text-[24px] md:text-[55px]  font-medium font-sans hover:text-sky-400 duration-500  z-10'>Visakha Consumer Products </h1>
+          <h1 className='text-black   text-[30px] md:text-[55px]  font-medium font-sans hover:text-sky-400 duration-500  z-10'>Visakha Consumer Products </h1>
+          
           <h2 className='   text-[14px]  md:text-[40px] text-black/10 mt-10 font-extrabold hover:text-red-600 hover:underline duration-500' >15.01.2024</h2>  
 
           <div className="flex gap-4 justify-center md:justify-start mt-10  z-10">
@@ -38,7 +40,7 @@ const Herobanner = () => {
         <div className='bg-gray-950  w-full md:w-[50%]  h-[100%]   flex flex-col gap-2 items-center justify-center'>
           
         <h1 className='text-red-500  text-[30px] lg:text-[60px] font-extrabold  text-center hover:text-white duration-500  z-10  ' >Elevate Your Home Hygiene with Precision</h1>
-        <p className='text-white text-center text-[12px]  lg:text-xl  leading-8 z-10 font-serif  hover:leading-9 duration-700  hover:bg-red-500/25 '>
+        <p className='text-white text-center text-[15px]  lg:text-xl  leading-8 z-10 font-serif  hover:leading-9 duration-700  hover:bg-black font-semibold  p3  '>
         At Visakha, we specialize in providing high-quality house disinfectants and washing machine liquids that ensure a clean and hygienic living environment for you and your family. Our commitment to excellence is reflected in every product we offer, as we prioritize your well-being and the cleanliness of your home. With a focus on innovation and effectiveness, Visakha Consumer Products brings you a range of carefully crafted solutions to meet all your household cleaning needs. Explore our products and discover the assurance of a healthy, sanitized, and refreshing home with Visakha Consumer Products.
         </p>
 
