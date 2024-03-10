a3c01b2128b42af811f487a6e2ecbf2b2ca5a8ea
vaibhav2sharma <108828319+vaibhav2sharma@users.noreply.github.com>
2024-03-10 14:17:01 +0530

    new commit

---
+++ src/app/components/Eyes.jsx
@@ -0,0 +1,67 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+
+
+const Eyes = () => {
+  const [rotate, setRotate] = useState(0);
+
+  useEffect(() => {
+   
+    const handleMouseMove = (e) => {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+
+      let deltaX = mouseX - window.innerWidth / 2;
+      let deltaY = mouseY - window.innerHeight / 2;
+
+      var angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+      setRotate(angle - 180);
+      
+    };
+
+    if (typeof window !== 'undefined'){
+      window.addEventListener('mousemove', handleMouseMove);
+    }
+
+    return() =>{
+      if(typeof window !== 'undefined'){
+        window.removeEventListener('mousemove', handleMouseMove);
+      }
+    }
+
+
+   
+
+    
+  }, []); // Empty dependency array ensures the effect runs only once after the initial render
+
+  return (
+    <div  className="w-full relative h-[30vh] md:h-[70vh] ">
+    <img
+      className="object-cover w-full"
+      src="https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg"
+      alt=""
+    />
+
+    <div className="bg-white h-[80px] w-[80px]  md:h-[200px]  md:w-[200px] absolute md:top-[300px] top-[70px] left-[20%] md:left-[35%] rounded-[50%]  ">
+      <div className="bg-black h-[60px] w-[60px] md:h-[100px] md:w-[100px] rounded-[50%] absolute md:top-[50px] md:left-[50px] top-[10px] left-[10px]">
+        <div style={{ transform: `rotate(${rotate}deg)` }} className="h-[20px] w-full absolute top-[39%]">
+          <div className="h-[20px] w-[20px] rounded-[50%] bg-white"></div>
+        </div>
+      </div>
+    </div>
+
+    <div className="bg-white h-[80px] w-[80px] md:h-[200px]  md:w-[200px] absolute md:top-[300px] top-[70px] right-[20%] md:right-[35%] rounded-[50%]  ">
+      <div className="bg-black h-[60px] w-[60px] md:h-[100px] md:w-[100px] rounded-[50%] absolute md:top-[50px] top-[10px] right-[10px] md:left-[50px]">
+        <div style={{ transform: `rotate(${rotate}deg)` }} className="h-[20px] w-full  absolute top-[39%] ">
+          <div className="h-[20px] w-[20px] rounded-[50%] bg-white"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default Eyes;
+
+
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { CiShoppingCart } from "react-icons/ci";
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between  bg-[#1E3A8A]   text-lg md:text-black  items-center sticky w-full h-[50px] md:h-[80px]   z-20 top-0 transition-transform duration-300  '>
+    <div className='flex justify-between  bg-[#FFFFFF]   text-lg md:text-black  items-center sticky w-full h-[50px] md:h-[80px]   z-20 top-0 transition-transform duration-300   '>
       
         <Link href='/'>
         <img src="/visakhalogo2.png" alt=""  className='w-[35px]  md:w-[40px] md:h-[60px] ml-2'/>
@@ -15,13 +15,15 @@ const Navbar = () => {
 
         <div className=' flex flex-row gap-4 '>
             <Link href='/about'>
-           <h2 className=' hover:text-white md:text-[25px] duration-500 sm:text-sm '>About</h2>
+           <h2 className=' md:text-[25px] sm:text-sm h-[2px] hover:underline transition-all duration-1000  '>About</h2>
+
+           
            </Link>
            <Link href='/contact'>
-           <h2 className=' hover:text-white md:text-[25px] duration-500 sm:text-sm  ' >Contact</h2>
+           <h2 className=' md:text-[25px]  sm:text-sm  hover:underline transition-all duration-1000  ' >Contact</h2>
            </Link>
            <Link href='/product'>
-           <h2 className=' hover:text-white md:text-[25px] duration-500 sm:text-sm  ' >Products</h2>
+           <h2 className=' md:text-[25px]  sm:text-sm  hover:underline transition-all duration-1000  ' >Products</h2>
            </Link>
            
         </div>
@@ -30,11 +32,11 @@ const Navbar = () => {
 
         <div>
             
-            <div className=' rounded-full mr-7 flex gap-1 hover:text-white duration-500'>
+            <div className=' rounded-full mr-7 flex gap-1  duration-500'>
            
             <Link href='/'>
             <CiShoppingCart className=' text-[19px] md:text-[34px] '/>
-            <h2 className='text-black text-[20px]' >Cart</h2>
+            <h2 className='text-black text-[20px] hover:underline transition-all duration-1000 ' >Cart</h2>
             </Link>
             </div>
 
