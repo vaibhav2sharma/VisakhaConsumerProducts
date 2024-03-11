ccedf1d2321b0d0bdd3361875a264f355e6f287d
vaibhav2sharma <108828319+vaibhav2sharma@users.noreply.github.com>
2024-03-11 12:13:48 +0530

    new files added

--- src/app/components/MArquee.jsx
+++ src/app/components/MArquee.jsx
@@ -4,10 +4,10 @@ import Marquee from "react-fast-marquee";
 
 const MArquee = () => {
   return (
-    <div  data-scroll data-scroll-speed="-.1"  className='w-full h-[50vh] md:h-[80vh] bg-[#004D43] rounded-b-[30px] flex items-center'>
+    <div  data-scroll data-scroll-speed="-.1"  className='w-full h-[50vh] md:h-[80vh] bg-red-600  rounded-b-[30px] flex items-center'>
 
     <Marquee className='text-white text-[100px] md:text-[200px] font-bold pt-[10px] md:pt-[60px]' speed={200} loop={0} >
-    <h1 className=' border-t-2 border-white border-b-2 items-center pt-9 uppercase'> Visakha consumer Products</h1>
+    <h1 className=' border-t-2 border-white border-b-2 items-center pt-9 uppercase'> Launching soon ......     </h1>
     </Marquee>
         
       
