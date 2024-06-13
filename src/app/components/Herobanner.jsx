e5cc2349fc60b4ca217dd550ac151181d034561a
vaibhav2sharma <108828319+vaibhav2sharma@users.noreply.github.com>
2024-06-13 19:51:31 +0530

    hero banner and eyes filles fixed

--- src/app/components/Herobanner.jsx
+++ src/app/components/Herobanner.jsx
@@ -6,16 +6,16 @@ import React from 'react'
 const Herobanner = () => {
   return (
    
-    <section data-scroll data-scroll-speed='-.1' className=' w-full h-[150vh] md:h-[185vh] overflow-hidden   '>
+    <section data-scroll data-scroll-speed='-.1' className=' w-full  xs:h-[170vh] md:h-[185vh] overflow-hidden   '>
 
         
-        <div className='bg-[#F1F1F1]  w-full md:w-[100%]   md:h-[100%] rounded-t-[30px]  md:rounded-[40px]  flex flex-col md:flex-row'>
+        <div className='bg-[#F1F1F1]  w-full md:w-[100%]   md:h-[100%]   md:rounded-[40px]  flex flex-col md:flex-row'>
           
 
           <div className='h-[100%] w-full   flex flex-col items-center justify-center '>
 
 
-          <h1 className=' text-[30px] md:text-[80px] md:leading-[0] md:tracking-tighter  text-[#374151] drop-shadow-lg"'>Visakha Detergent</h1>
+          <h1 className=' text-[30px] md:text-[80px] md:leading-[0] md:tracking-tighter  text-[#374151] drop-shadow-lg pt-6 md:pt-0 "'>Visakha Detergent</h1>
 
           <p className=' text-[#374151] text-center w-[70%] md:w-[45%] pt-[50px]'>
           Discover the power of pure clean with Visakha Detergent! Our innovative formula tackles tough stains effortlessly, leaving your clothes brilliantly clean and vibrant after every wash. Gentle on fabrics and kind to the planet, Visakha Detergent ensures a fresh, radiant finish that you can see and feel. Elevate your laundry routine with Visakha where every wash is a promise of sparkling cleanliness and unmatched freshness. Try it today and experience the magic of true cleanliness!
@@ -36,7 +36,7 @@ const Herobanner = () => {
           </div>
           <div className=' h-[100%] w-full bg-gray-800 flex flex-col  justify-center items-center'>
 
-            <h3 className='text-[30px] md:text-[80px] md:leading-[0] md:tracking-tighter  text-[#f1f1f1] drop-shadow-lg'> Dirty Clothes? </h3>
+            <h3 className='text-[30px] md:text-[80px] md:leading-[0] md:tracking-tighter  text-[#f1f1f1] drop-shadow-lg   md:pt-0'> Dirty Clothes? </h3>
             
             <img
             className='px-2 '
