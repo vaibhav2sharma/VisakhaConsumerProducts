9b1013c57730a76bc1c227eebb30ffd5e7ea457d
vaibhav2sharma <108828319+vaibhav2sharma@users.noreply.github.com>
2024-12-13 02:25:34 +0530

    marquee added

--- src/app/page.js
+++ src/app/page.js
@@ -7,10 +7,11 @@ import store from "./store/store";
 import Footer from "./components/Footer";
 import Herobanner from "./components/Herobanner";
 import LandingText from "./components/LandingText";
-import Marquee from "./components/Marquee";
+
 import Navbar from "./components/Navbar";
 import Eyesffile from "./components/Eyesffile";
 import ProductComponentShow from "./components/ProductComponentShow";
+import MArquee from "./components/MArquee";
 
 const { default: Link } = require("next/link");
 
@@ -27,7 +28,7 @@ export default function Home() {
       <div className="bg-[#F1F1F1] font-[my-font]">
         <Navbar />
         <LandingText />
-        <Marquee />
+        <MArquee />
         <Herobanner />
         <div className="bg-gradient-to-b from-[#f1f1f1] to-[#81CFF5]">
           <Eyesffile />
