8e902fc87a7fea733d32540c8596a44eb416ccfc
vaibhav2sharma <108828319+vaibhav2sharma@users.noreply.github.com>
2024-12-13 02:14:31 +0530

    agro removed

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import Carousel from "./components/Carousel";
+
 import Footer from "./components/Footer";
 import Herobanner from "./components/Herobanner";
 import LandingText from "./components/LandingText";
@@ -11,7 +11,6 @@ import Marquee from "./components/Marquee";
 import Navbar from "./components/Navbar";
 import Eyesffile from "./components/Eyesffile";
 import ProductComponentShow from "./components/ProductComponentShow";
-import ProductComponentShowAgro from "./components/ProductComponentShowAgro";
 
 const { default: Link } = require("next/link");
 
@@ -33,7 +32,7 @@ export default function Home() {
         <div className="bg-gradient-to-b from-[#f1f1f1] to-[#81CFF5]">
           <Eyesffile />
           <ProductComponentShow />
-          {/* <ProductComponentShowAgro /> */}
+
           <Footer />
         </div>
       </div>
